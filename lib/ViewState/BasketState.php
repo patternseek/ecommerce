08cf221dfa8d34cc4cf1738a7dc7355865f15d3f
Tolan Blundell <tolan@patternseek.net>
2024-07-18 18:47:58 +0200

    indentatino

--- lib/ViewState/BasketState.php
+++ lib/ViewState/BasketState.php
@@ -277,11 +277,10 @@ class BasketState extends ViewState
      */
     public function getVatRate( $countryCode )
     {
-	if(empty($countryCode)){
-           return 0.0;
-	}
-        $ucCountryCode = mb_strtoupper( $countryCode,
-            'UTF-8' );
+        if(empty($countryCode)){
+               return 0.0;
+        }
+        $ucCountryCode = mb_strtoupper( $countryCode, 'UTF-8' );
         if (!isset( $this->vatRates[ 'rates' ][ $ucCountryCode ] )) {
             return 0.0;
         }
